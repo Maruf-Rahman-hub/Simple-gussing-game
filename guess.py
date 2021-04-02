098df3eb06473bef10c2415f6cf4a6b180001f4c
Maruf-Rahman-hub <70712460+Maruf-Rahman-hub@users.noreply.github.com>
2021-04-02 05:59:51 -0700

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,10 +1,16 @@
 import random
-num = random.randint(1 , 10)
+User = input("Enter Your name: ")
+print(f"Hello {User} welcome to a guess game\n!!!WISH YOU  BEST OF LUCK!!! \n****************************")
+comp = random.randint(1,10) 
 for i in range(3):
-    guess = int(input("Enter a number :"))
-    if guess > num:
-        print("Sorry your number is greater!")
-    elif guess < num:
-        print("Sorry your number is lower!")
-    else:
-        print("You are correct")        
+    player = int(input("Enter a number between 1 to 10: "))
+        
+    if player == comp:
+        print(f"You gussed correct number.computer chose {comp}")
+        break#if the user entered a correct num it will break the loop
+    elif player > comp:
+        print(f"You gussed uncorrect number,number is lower than {player}")
+    elif player < comp:
+        print(f"You gussed uncorrect number,number is higher than {player}") 
+print(f"the number was {comp}")
+            
